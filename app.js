2887be8fd7afc34f5b1fc778f121d6278255139c
ShaikMahmoodSameer <mahmoodsameershaik@gmai.com>
2023-07-01 17:16:07 +0530

    c3

--- app.js
+++ app.js
@@ -1,310 +1,53 @@
-const testsarr = [
-{
-    "testId" : "INV2", 
-    "testTitle" : "10X300 SWAB C/S", 
-    "testPrice" : 3000
-},
-{
-    "testId" : "INV3", 
-    "testTitle" : "17-Ketosteroids", 
-    "testPrice" : 1700
-},
-{
-    "testId" : "INV4", 
-    "testTitle" : "17-OH Progesterone", 
-    "testPrice" : 2500
-},
-{
-    "testId" : "INV5", 
-    "testTitle" : "17X300 SWAB C/S..", 
-    "testPrice" : 5100
-},
-{
-    "testId" : "INV6", 
-    "testTitle" : "22X300 OT CULTURE & SENSITIVITY", 
-    "testPrice" : 6600
-},
-{
-    "testId" : "INV2424", 
-    "testTitle" : "24 Hours Urinary Microalbumin Creatinine Ratio", 
-    "testPrice" : 2000
-},
-{
-    "testId" : "INV2426", 
-    "testTitle" : "24 Hours Urine Potassium Excretion", 
-    "testPrice" : 450
-},
-{
-    "testId" : "INV2425", 
-    "testTitle" : "24 Hours Urine Sodium Excretion", 
-    "testPrice" : 450
-},
-{
-    "testId" : "INV7", 
-    "testTitle" : "24 HRS Electrolytes - Urine", 
-    "testPrice" : 1200
-},
-{
-    "testId" : "INV8", 
-    "testTitle" : "24 Hrs Urinary Copper", 
-    "testPrice" : 1000
-},
-{
-    "testId" : "INV9", 
-    "testTitle" : "24 Hrs Urinary Cortisol", 
-    "testPrice" : 1500
-},
-{
-    "testId" : "INV10", 
-    "testTitle" : "24 Hrs Urinary Creatinine", 
-    "testPrice" : 1000
-},
-{
-    "testId" : "INV11", 
-    "testTitle" : "24 Hrs Urinary Creatinine Clearence", 
-    "testPrice" : 1000
-},
-{
-    "testId" : "INV12", 
-    "testTitle" : "24 Hrs Urinary Metanephrines", 
-    "testPrice" : 3500},
-{
-    "testId" : "INV13", 
-    "testTitle" : "24 Hrs Urinary Microalbuminuria", 
-    "testPrice" : 1000
-},
-{
-    "testId" : "INV14", 
-    "testTitle" : "24 Hrs Urinary Osmolality", 
-    "testPrice" : 1800
-},
-{
-    "testId" : "INV15", 
-    "testTitle" : "24 Hrs Urinary Phosphorus", 
-    "testPrice" : 1500
-},
-{
-    "testId" : "INV16", 
-    "testTitle" : "24 Hrs Urinary Potassium", 
-    "testPrice" : 1000
-},
-{
-    "testId" : "INV17", 
-    "testTitle" : "24 Hrs Urinary Prophobilinogen (Qualitative)", 
-    "testPrice" : 2000
-},
-{
-    "testId" : "INV18", 
-    "testTitle" : "24 Hrs Urinary Urea", 
-    "testPrice" : 600
-},
-{
-    "testId" : "INV19", 
-    "testTitle" : "24 HRS URINE PROTEIN CREATINE RATIO", 
-    "testPrice" : 600
-},
-{
-    "testId" : "INV1937", 
-    "testTitle" : "24 Hrs Urine VMA(Vanillyl Mandelic Acid)", 
-    "testPrice" : 2000
-},
-{
-    "testId" : "INV20", 
-    "testTitle" : "24 HRS- URINE CALCIUM", 
-    "testPrice" : 1000
-},
-{
-    "testId" : "INV85", 
-    "testTitle" : "24Hrs Urine Protein/Creatinine Ratio", 
-    "testPrice" : 600
-},
-{
-    "testId" : "INV22", 
-    "testTitle" : "32X300 SWAB C/S-I", 
-    "testPrice" : 9600
-},
-{
-    "testId" : "INV23", 
-    "testTitle" : "32X300 SWAB C/S-II", 
-    "testPrice" : 9600
-},
-{
-    "testId" : "INV24", 
-    "testTitle" : "37X300 CULTURE & SENSITIVITY", 
-    "testPrice" : 11100
-},
-{
-    "testId" : "INV25", 
-    "testTitle" : "5-Hydroxyindoleacetic acid (5-HIAA)", 
-    "testPrice" : 2500
-},
-{
-    "testId" : "INV26", 
-    "testTitle" : "5X300 CULTURE & SENSITIVITY", 
-    "testPrice" : 1500
-},
-{
-    "testId" : "INV27", 
-    "testTitle" : "7X300 SWAB C/S", 
-    "testPrice" : 2100
-},
-{
-    "testId" : "INV28", 
-    "testTitle" : "8X400 SWAB FOR CULTURE & SENSITIVIT", 
-    "testPrice" : 3200
-},
-{
-    "testId" : "INV29", 
-    "testTitle" : "ABG WITH ELECTROLYTE", 
-    "testPrice" : 1500
-},
-{
-    "testId" : "INV30", 
-    "testTitle" : "Absolute Basophil Count", 
-    "testPrice" : 200
-},
-{
-    "testId" : "INV31", 
-    "testTitle" : "Absolute Eosinophil Count", 
-    "testPrice" : 200
-},
-{
-    "testId" : "INV32", 
-    "testTitle" : "Absolute Granulocyte Count", 
-    "testPrice" : 200
-},
-{
-    "testId" : "INV33", 
-    "testTitle" : "Absolute Lymphocyte Count", 
-    "testPrice" : 200
-},
-{
-    "testId" : "INV34", 
-    "testTitle" : "Absolute Monocyte Count", 
-    "testPrice" : 200
-},
-{
-    "testId" : "INV35", 
-    "testTitle" : "Absolute Neutrophil Count", 
-    "testPrice" : 200},
-{
-    "testId" : "INV36", 
-    "testTitle" : "Acetaminophen (Paracetamol) Level", 
-    "testPrice" : 800
-},
-{
-    "testId" : "INV37", 
-    "testTitle" : "Acetone", 
-    "testPrice" : 800
-},
-{
-    "testId" : "INV38", 
-    "testTitle" : "Acetyl Chloine Receptor Antibody(AchR)", 
-    "testPrice" : 3500
-},
-{
-    "testId" : "INV39", 
-    "testTitle" : "Acetylcholinesterase", 
-    "testPrice" : 1000
-},
-{
-    "testId" : "INV40", 
-    "testTitle" : "Acid Fast Suspectibility - 1 st line Drugs", 
-    "testPrice" : 9500
-},
-{
-    "testId" : "INV41", 
-    "testTitle" : "Acid Fast Suspectibility - 1st & 2nd line Drugs", 
-    "testPrice" : 12000
-},
-{
-    "testId" : "INV42", 
-    "testTitle" : "Acid Fast Suspectibility - 5 Drugs", 
-    "testPrice" : 3000
-},
-{
-    "testId" : "INV43", 
-    "testTitle" : "Acid Phosphatase - Prostatic Fraction", 
-    "testPrice" : 1000
-}];
-
-
 const express = require("express");
 const bodyParser = require("body-parser");
-const dotenv = require('dotenv');
-const https = require('https');
+const dotenv = require("dotenv");
+const https = require("https");
 const { log } = require("console");
-const mysql = require('mysql');
+const mysql = require("mysql");
 
 const app = express();
-app.use( bodyParser.urlencoded({extended: true}) );
-dotenv.config({path: 'config.env'});
+app.use(bodyParser.urlencoded({ extended: true }));
+dotenv.config({ path: "config.env" });
 
 // database conn
 const con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "konnect_db0" 
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "konnect_db0",
 });
 
-function add_into_mysqldb_tests_tests(test_id,test_title,test_price){
-    con.connect(function(err){
-        if(err){
-            throw err;
-        }else{
-            console.log('mysql DB connected');
-            var sql = "INSERT into tests (test_id, test_title, test_price) VALUES ?";
-            var values = [[test_id,test_title,test_price]];
-            con.query(sql, [values], function(err, result){
-                if(err) throw err;
-                console.log('Records inserted into db: '+result.affectedRows);
-            }) 
-        }
-    })
+function add_into_mysqldb_tests_tests(test_id, test_title, test_price) {
+  con.connect(function (err) {
+    if (err) {
+      throw err;
+    } else {
+      console.log("mysql DB connected");
+      var sql = "INSERT into tests (test_id, test_title, test_price) VALUES ?";
+      var values = [[test_id, test_title, test_price]];
+      con.query(sql, [values], function (err, result) {
+        if (err) throw err;
+        console.log("Records inserted into db: " + result.affectedRows);
+      });
+    }
+  });
 }
 
-
 // for(var i = 1; i < tests.length; i++){
-        //     
-        // }  
-
-// for(var i=0; i<testsarr.length; i++){
-    add_into_mysqldb_tests_tests(
-        testsarr[0].testId,
-        testsarr[0].testTitle,
-        testsarr[0].testPrice
-    );
+//
 // }
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname +'/index.html');
-})
-app.post('/', (req, res) => {
-    // const test_id = req.body.test_id;
-    // const test_title = req.body.test_title;
-    // const test_price = req.body.test_price;
-    // for(var i=0; i<testsarr.length; i++){
-        // add_into_mysqldb_tests_tests(
-        //     testsarr[0].testId,
-        //     testsarr[0].testTitle,
-        //     testsarr[0].testPrice
-        // );
-    // }   
-})
 
-const PORT = process.env.PORT  || 8080;
+// add_into_mysqldb_tests_tests(a,b,c);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+app.post("/", (req, res) => {
+  
+});
+
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-    console.log(`App running on port http://localhost:${PORT}`);
-})
-
-
-
-
-
-
-
-
-
-
-
+  console.log(`App running on port http://localhost:${PORT}`);
+});
